db96252a9b5cdabab9f8e2c1836fa797c659e8b8
bekaa <sci.kd.eg@gmail.com>
2017-10-07 14:57:04 +0200

    beta version

--- js/listener.js
+++ js/listener.js
@@ -7,6 +7,4 @@ function openPage()
 		});
 }
 
-
-
 browser.browserAction.onClicked.addListener(openPage);
--- js/set-bookmarks-dict.js
+++ js/set-bookmarks-dict.js
@@ -1,151 +1,103 @@
-var linkIcon = browser.extension.getURL("/icons/link.png");
-var folderIcon = browser.extension.getURL("/icons/folder.png");
-//var bookmarksData = [];
-//var itemDict = new Map();
-
-function getRoot(bookmarkItems)
+var PARENT
+var ROOT
+var TRACKER = []
+function logRoot(bookmarkItems)
 {
-	if(bookmarkItems[0].children)
+	PARENT = bookmarkItems[0].children.find(function(child)
 	{
-		for( let child of bookmarkItems[0].children)
-		{
-			if( child.title == 'Bookmarks Menu' )
-			{
-				var parent = child;
-				break;
-			}
-
-		}
-	}
-	logTree(parent);
+		return child.title == "Bookmarks Menu";
+	});
+	ROOT = PARENT;
+	TRACKER = [];
+	TRACKER.push(PARENT);
+	logTree();
 }
-function getParent(parentID)
+//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+function logNode(parentID)
 {
-	let parent = browser.bookmarks.getSubTree(parentID);
-	//debugger;
-	parent.then(function(b){logTree(b[0]);}, onRejected);
+	let parent = PARENT.children.find(function(child)
+	{
+		return child.id == parentID;
+	});
+	if (parent)
+	{
+		PARENT = parent;
+		TRACKER.push(PARENT)
+		logTree();
+	}
+	else { onRejected("No nodes with id " + parentID); }
 }
+//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+function goBack()
+{
+	if(!PARENT) { return; }
+	if( PARENT.parentId == "root________" ) { return; }
+	if(TRACKER == [] || TRACKER == [ROOT] ) { return; }
+
+	TRACKER.pop();
+	PARENT = TRACKER[TRACKER.length - 1];
+	logTree();
 
 
-function logTree(parent)
+}
+//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+function logTree()
 {
 	//remove all current items if there is
 	$(".pb-filemng-template-body").empty();
+	$('.currentNode').empty();
+	$(".currentNode").append('<p class="title">' + PARENT.title + '</p>');
+
 	// define two important containers.
 	var bookmarksData = [];
-	var itemDict = new Map();
-	debugger;
+	//debugger;
 	// get children.
-	for ( let child of parent.children )
+	for ( let child of PARENT.children )
 	{
-		if (child.url)
-		{
-			itemDict.set(child.id, [ child.url, child.title, "link" ] );
-		}
-		else
-		{
-			itemDict.set(child.id, [ "", child.title, "folder" ] );
-		}
-	}
 	//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 	// update bokmarks Data list.
 	//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	for( let entry of itemDict.entries() )
-	{
-		// loop through all bookmarks items in shape [id],[ url, title, "link" or "folder" ]
-		// set values to the three variables, url, title and icon.
-		let iid = entry[0];
-		let iurl = entry[1][0];
-		let ititle = entry[1][1];
-		let itype  = entry[1][2];
-
 		let id;
 		let url;
 		let title;
+		let link;
 		let icon;
+		let textColor;
 
-		if( itype == "link" )
+		if( child.url ) // if a link.
 		{
-			icon = linkIcon;
-			url = iurl;
+			url = child.url;
 			id  = "";
-			if( ititle == "" )
-			{
-				title = iurl;
-			}
-			else
-			{
-				title = ititle;
-			}
+			link = "href=\"" + url + "\" target=\"_blank\"";
+			if( child.title == "" ) { title = url; }
+			else { title = child.title; }
+			icon = "<span class=\"glyphicon glyphicon-link\"></span>";
+			textColor = 'style=\"color:red;\"';
 		}
-		else
+		else // if a folder.
 		{
-			icon = folderIcon;
-			title = ititle;
+			title = child.title;
 			url = "";
-			id = iid
+			id = child.id
+			link = "id=\"" + id + "\"";
+			icon = "<span class=\"glyphicon glyphicon-folder-open\"></span>";
+			textColor = 'style=\"color:blue;\"';
 		}
-		// update the bookmarksData list.
-		bookmarksData.push(
+
+		$(".pb-filemng-template-body").append( "<a " + link + " > " +
+			'<div class=\"col-xs-6 col-sm-6 col-md-6 pb-filemng-body-folders \">' + icon +
+			'<p class="pb-filemng-paragraphs" '+ textColor +'>' +
+			title + '</p> </div> </a>'
+		);
+
+		$( "#" + id  ).click(function()
 		{
-			"icon":
-			"<span class=\"glyphicon glyphicon-folder-open\"></span>",
-			"title": title,
-			"url": url,
-			"id": id
+			logNode(id);
 		});
+
 	}
-	//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// using JQuery update tags for bootstrap to make awsomeness showup.
-	//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	$(function ()
-	{
-		for ( var item of bookmarksData )
-		{
-			if( item.id == "" )
-			{
-				var link = "href=\"" + item.url + "\"";
-			}
-			else
-			{
-				var link = "id=\"" + item.id + "\"";
-
-			}
-
-			var div = $(".pb-filemng-template-body").append(
-				"<a " + link + " > " +
-				'<div class=\"col-xs-6 col-sm-6 col-md-6 pb-filemng-body-folders\">' +
-				item.icon +
-				'<p class="pb-filemng-paragraphs">' + item.title + '</p></a>' + '</div>'
-			);
-			//debugger;
-			if( item.id != "" )
-			{
-				//debugger;
-				//getParent(item.id)
-				//document.getElementById(item.id).addEventListener("click", function()
-				//{
-				//	console.log("hello");
-					//getParent(item.id);
-				//}, onRejected);
-/*
-				div.promise().done(function()
-				{
-					console.log("done");
-					$( "#" + item.id  ).click(function()
-					{
-						console.log("hello");
-						debugger;
-						getParent(item.id);
-						return false;
-					});
-				});*/
-			}
-		}
-	})
-	//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	//-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 }
+
 
 function onRejected(error)
 {
@@ -154,4 +106,9 @@ function onRejected(error)
 
 //-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 var gettingTree = browser.bookmarks.getTree();
-gettingTree.then(getRoot, onRejected);
+gettingTree.then(logRoot, onRejected);
+
+$( ".glyphicon-menu-left" ).click(function()
+{
+	goBack();
+});
